6df4080ba9e47744917badb0de18e085dc70c7f5
s1mpletop1 <649407921@qq.com>
2019-10-13 00:12:45 -0400

    第一个

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("ddd");
+        System.out.println(" 这是第一个分支");
+    }
+}
---
+++ src/branch1.java
@@ -0,0 +1,8 @@
+public class branch1 {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("2r1441");
+        System.out.println("rsfc");
+        System.out.println(" 这是第一个分支");
+    }
+}
